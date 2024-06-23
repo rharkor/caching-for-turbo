93a790e158cf965b89d16848db776981288f642e
rharkor <louis@huort.com>
2024-06-23 14:55:59 +0200

    fix: post

--- post.js
+++ post.js
@@ -24,6 +24,7 @@ async function run() {
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
+    throw error
   }
 }
 
